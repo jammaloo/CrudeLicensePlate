32f29987d6576feef106911cc5464873aee27c52
James Freeman <jmcfreeman@gmail.com>
2019-03-24 22:50:42 -0400

    Add basic working version of generator

--- license.js
+++ license.js
@@ -1,14 +1,46 @@
+class LicenseSwearer {
+    constructor(wordlist, rules) {
+        this.wordlist = wordlist || [];
+        this.rules = rules || {};
+        this.swearIndex = 0;
+        this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        this.alphabetSubstitute = 'A___3__HI__1MNO_Q_2TUVWXY501SE_Z__8_';
+    }
+
+    convert(word) {
+        let newWord = '';
+        for (let i = 0; i < word.length; i++) {
+            let index = this.alphabet.indexOf(word[i]);
+            let newLetter = this.alphabetSubstitute[index];
+            if (newLetter === '_') {
+                console.log(word);
+                return null;
+            }
+            newWord += newLetter;
+        }
+        return newWord.split("").reverse().join("");
+    }
+
+    generate() {
+        for (let i = 0; i < this.wordlist.length; i++) {
+            const currentWord = this.convert(this.wordlist[this.swearIndex++].toUpperCase());
+
+            if (currentWord) {
+                return currentWord;
+            }
+        }
+        return 'FAILED';
+    }
+}
+
 function setPlate(license) {
     document.getElementById('license_plate').innerText = license;
 }
 
-function generate() {
-    setPlate('Banana');
-}
-
-function init() {
-    setPlate('Some Plate');
-    document.getElementById('refresh_plate').onclick = generate;
-}
-
-init();
+(function init() {
+    const swearer = new LicenseSwearer(naughtyWords, {})
+    document.getElementById('refresh_plate').onclick = function() {
+        setPlate(swearer.generate());
+    };
+    setPlate(swearer.generate());
+})();
